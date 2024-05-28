cee8f05812a5a705bc2dcf2de10ee5db85746d45
Alex Roberts <git@rbrts.io>
2024-05-28 09:27:55 -0700

    Cleanup, and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elektron LFO Calculator
+
+A tool to help you decipher what _SPD_ and _MULT_ in Elektron LFOs are doing. And calculate the perfect settings for the LFO duration you want.
--- calc.js
+++ calc.js
@@ -125,164 +125,167 @@ const multipliersData = [
   },
 ];
 
-createApp({
-  setup() {
-    const bpm = ref(120);
-    const beatDuration = ref(0.5);
-    const barDuration = ref(2);
+function init() {
+  createApp({
+    setup() {
+      const bpm = ref(120);
+      const beatDuration = ref(0.5);
+      const barDuration = ref(2);
 
-    const lfoSpeed = ref(32.0);
-    const multipliers = ref(multipliersData);
-    const multiplier = ref(5);
+      const lfoSpeed = ref(32.0);
+      const multipliers = ref(multipliersData);
+      const multiplier = ref(5);
 
-    const time = ref();
-    const rateOut = ref();
-    const hertz = ref();
+      const time = ref();
+      const rateOut = ref();
+      const hertz = ref();
 
-    function onBpmInput(e) {
-      bpm.value = e.target.value;
+      function onBpmInput(e) {
+        bpm.value = e.target.value;
 
-      // Assuming 4/4 time.
-      // TODO: Check if the pattern scaling affects LFO timing?
-      beatDuration.value = 60 / bpm.value;
-      barDuration.value = beatDuration.value * 4;
-      calculateRates();
-    }
-
-    function onLfoSpeedInput(e) {
-      lfoSpeed.value = e.target.value;
-      calculateRates();
-    }
-
-    function onMultiplierChange(e) {
-      multiplier.value = parseInt(e.target.value);
-      calculateRates();
-    }
-
-    function onTimeInput(e) {
-      time.value = parseFloat(e.target.value);
-      const res = calculateSpeedFromTime(time.value);
-
-      console.log(`${res.speed}; multiplier: ${multipliersData[res.multiplier].mult}`);
-
-      if (res.speed <= 64) {
-        lfoSpeed.value = res.speed;
-        multiplier.value = res.multiplier;
+        // Assuming 4/4 time.
+        // TODO: Check if the pattern scaling affects LFO timing?
+        beatDuration.value = 60 / bpm.value;
+        barDuration.value = beatDuration.value * 4;
         calculateRates();
-      } else {
-        lfoSpeed.value = "";
-        hertz.value = "";
-        rateOut.value = "";
       }
-    }
 
-    function onHertzInput(e) {
-      hertz.value = parseFloat(e.target.value);
-      const res = calculateSpeedFromTime(1 / hertz.value);
-
-      console.log(`${res.speed}; multiplier: ${multipliersData[res.multiplier].mult}`);
-
-      if (res.speed <= 64) {
-        lfoSpeed.value = res.speed;
-        multiplier.value = res.multiplier;
+      function onLfoSpeedInput(e) {
+        lfoSpeed.value = e.target.value;
         calculateRates();
-      } else {
-        lfoSpeed.value = "";
-        time.value = "";
-        rateOut.value = "";
       }
-    }
 
-    function calculateRates() {
-      const index = multiplier.value;
-      rateOut.value = calculateNotationFor(index);
-      time.value = sensibleFixedNum(calculateTimeFor(index));
-      hertz.value = calculateHertzFor(index);
-    }
+      function onMultiplierChange(e) {
+        multiplier.value = parseInt(e.target.value);
+        calculateRates();
+      }
 
-    // While this function is called a lot, there are only 24 index options
-    // I don't really feel the need to cache this, your computer, phone, what-have-you can handle it.
-    function calculateRateFor(multiplierIndex) {
-      let x = lfoSpeed.value * speedMultiplier;
-      return 1 / x / multipliersData[multiplierIndex].mult;
-    }
+      function onTimeInput(e) {
+        time.value = parseFloat(e.target.value);
+        const res = calculateSpeedFromTime(time.value);
 
-    function calculateSpeedFromTime(timeIn) {
-      let t = timeIn;
-      let o = 0;
-      let i = 0;
+        console.log(`${res.speed}; multiplier: ${multipliersData[res.multiplier].mult}`);
 
-      for (i = 0; i < 12; i++) {
-        let m = multipliersData[i];
-
-        o = 1 / ((t / barDuration.value) * m.mult) / speedMultiplier;
-        if (o <= 64) {
-          break;
+        if (res.speed <= 64) {
+          lfoSpeed.value = res.speed;
+          multiplier.value = res.multiplier;
+          calculateRates();
+        } else {
+          lfoSpeed.value = "";
+          hertz.value = "";
+          rateOut.value = "";
         }
       }
 
+      function onHertzInput(e) {
+        hertz.value = parseFloat(e.target.value);
+        const res = calculateSpeedFromTime(1 / hertz.value);
+
+        console.log(`${res.speed}; multiplier: ${multipliersData[res.multiplier].mult}`);
+
+        if (res.speed <= 64) {
+          lfoSpeed.value = res.speed;
+          multiplier.value = res.multiplier;
+          calculateRates();
+        } else {
+          lfoSpeed.value = "";
+          time.value = "";
+          rateOut.value = "";
+        }
+      }
+
+      function calculateRates() {
+        const index = multiplier.value;
+        rateOut.value = calculateNotationFor(index);
+        time.value = sensibleFixedNum(calculateTimeFor(index));
+        hertz.value = calculateHertzFor(index);
+      }
+
+      // While this function is called a lot, there are only 24 index options
+      // I don't really feel the need to cache this, your computer, phone, what-have-you can handle it.
+      function calculateRateFor(multiplierIndex) {
+        let x = lfoSpeed.value * speedMultiplier;
+        return 1 / x / multipliersData[multiplierIndex].mult;
+      }
+
+      function calculateSpeedFromTime(timeIn) {
+        let t = timeIn;
+        let o = 0;
+        let i = 0;
+
+        for (i = 0; i < 12; i++) {
+          let m = multipliersData[i];
+
+          o = 1 / ((t / barDuration.value) * m.mult) / speedMultiplier;
+          if (o <= 64) {
+            break;
+          }
+        }
+
+        return {
+          speed: o,
+          multiplier: i,
+        };
+      }
+
+      function sensibleFixedNum(input) {
+        return Math.round(input * 1000) / 1000;
+      }
+
+      function calculateNotationFor(multiplierIndex) {
+        const rate = calculateRateFor(multiplierIndex);
+
+        if (rate > 1) {
+          return `${sensibleFixedNum(rate)}/1`;
+        } else {
+          return `1/${sensibleFixedNum(1 / rate)}`;
+        }
+      }
+
+      function calculateTimeFor(multiplierIndex) {
+        const rate = calculateRateFor(multiplierIndex);
+
+        let duration = barDuration.value;
+        if (multipliersData[multiplierIndex].isFixed) {
+          // Fixed multiplier is based on 120 BPM
+          duration = 2;
+        }
+
+        return rate * duration;
+      }
+
+      function calculateHertzFor(multiplierIndex) {
+        return sensibleFixedNum(1 / calculateTimeFor(multiplierIndex));
+      }
+
+      calculateRates();
+
       return {
-        speed: o,
-        multiplier: i,
+        bpm,
+
+        beatDuration,
+        barDuration,
+
+        onLfoSpeedInput,
+        onMultiplierChange,
+        onBpmInput,
+        onTimeInput,
+        onHertzInput,
+
+        multipliers,
+        multiplier,
+        lfoSpeed,
+
+        time,
+        rateOut,
+        hertz,
+
+        sensibleFixedNum,
+        calculateNotationFor,
+        calculateHertzFor,
+        calculateTimeFor,
       };
-    }
-
-    function sensibleFixedNum(input) {
-      return Math.round(input * 1000) / 1000;
-    }
-
-    function calculateNotationFor(multiplierIndex) {
-      const rate = calculateRateFor(multiplierIndex);
-
-      if (rate > 1) {
-        return `${sensibleFixedNum(rate)}/1`;
-      } else {
-        return `1/${sensibleFixedNum(1 / rate)}`;
-      }
-    }
-
-    function calculateTimeFor(multiplierIndex) {
-      const rate = calculateRateFor(multiplierIndex);
-
-      let duration = barDuration.value;
-      if (multipliersData[multiplierIndex].isFixed) {
-        // Fixed multiplier is based on 120 BPM
-        duration = 2;
-      }
-
-      return rate * duration;
-    }
-
-    function calculateHertzFor(multiplierIndex) {
-      return sensibleFixedNum(1 / calculateTimeFor(multiplierIndex));
-    }
-
-    calculateRates();
-
-    return {
-      bpm,
-
-      beatDuration,
-      barDuration,
-
-      onLfoSpeedInput,
-      onMultiplierChange,
-      onBpmInput,
-      onTimeInput,
-      onHertzInput,
-
-      multipliers,
-      multiplier,
-      lfoSpeed,
-
-      time,
-      rateOut,
-      hertz,
-
-      sensibleFixedNum,
-      calculateNotationFor,
-      calculateHertzFor,
-      calculateTimeFor,
-    };
-  },
-}).mount("#app");
+    },
+  }).mount("#app");
+}
+window.addEventListener("load", () => init());
